73817bfbf11290d4620c6a68aac292cf1c1ec803
unknown <mcmanuel755@gmail.com>
2026-04-01 00:30:02 +0100

    added fields to book entity

--- src/main/java/com/example/BookSocialNetwork/entities/Books.java
+++ src/main/java/com/example/BookSocialNetwork/entities/Books.java
@@ -1,8 +1,47 @@
 package com.example.BookSocialNetwork.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 public class Books {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
 }
