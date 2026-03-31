075707a73f7b9794e808f8af25fd152a3e515ef0
unknown <mcmanuel755@gmail.com>
2026-04-01 00:55:16 +0100

    edited the book entity

--- src/main/java/com/example/BookSocialNetwork/entities/Books.java
+++ src/main/java/com/example/BookSocialNetwork/entities/Books.java
@@ -1,18 +1,18 @@
 package com.example.BookSocialNetwork.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @Entity
@@ -28,6 +28,17 @@ public class Books {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<FeedBack> feedBacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
 
     @CreatedDate
     @Column(nullable = false,updatable = false)
