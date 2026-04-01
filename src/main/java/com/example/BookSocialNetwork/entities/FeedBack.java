05265151264354311a165599e384ab48b35111fe
unknown <mcmanuel755@gmail.com>
2026-04-01 01:00:05 +0100

    added the feedback class

---
+++ src/main/java/com/example/BookSocialNetwork/entities/FeedBack.java
@@ -0,0 +1,44 @@
+package com.example.BookSocialNetwork.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@Entity
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class FeedBack {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String note;
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+
+    private Books books;
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
