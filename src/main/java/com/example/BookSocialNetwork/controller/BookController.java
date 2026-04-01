ab1d0481a540897b0f17d63ffa5e19a684abd997
unknown <mcmanuel755@gmail.com>
2026-04-01 03:06:35 +0100

    worked on the book controller class

--- src/main/java/com/example/BookSocialNetwork/controller/BookController.java
+++ src/main/java/com/example/BookSocialNetwork/controller/BookController.java
@@ -1,7 +1,28 @@
 package com.example.BookSocialNetwork.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.BookSocialNetwork.model.BookRequest;
+import com.example.BookSocialNetwork.model.BookResponse;
+import com.example.BookSocialNetwork.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser){
+        return ResponseEntity.ok(service.save(request,connectedUser));
+    }
+
+    @GetMapping("/{book-id}")
+    public ResponseEntity<BookResponse> findBookById(@Valid @PathVariable("book-id") Integer bookId){
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
 }
